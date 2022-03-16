9761ae6035f748dbf16096f27f4464481c8ba75a
smin9408 <stmdaks888@naver.com>
2022-03-17 04:57:10 +0900

    회원가입 API 추가 / 실제 호출

--- app/src/main/java/com/example/keepthetime/SignUpActivity.kt
+++ app/src/main/java/com/example/keepthetime/SignUpActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime.databinding.ActivitySignUpBinding
+import com.example.keepthetime.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
 
@@ -25,6 +29,17 @@ class SignUpActivity : BaseActivity() {
             val inputNickname = binding.edtNickname.text.toString()
 
 //            회원가입 API 호출 (PUT - "/user")
+            apiList.putRequestSignUp(inputEmail, inputPassword, inputNickname).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                }
+
+            })
 
         }
     }
