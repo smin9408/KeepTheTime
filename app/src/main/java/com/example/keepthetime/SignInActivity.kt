20bbeb132e70aaa30a9fc1e40592ec86d260b8e1
smin9408 <stmdaks888@naver.com>
2022-03-17 04:12:23 +0900

    주석 추가 runOnUiThread

--- app/src/main/java/com/example/keepthetime/SignInActivity.kt
+++ app/src/main/java/com/example/keepthetime/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.example.keepthetime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime.api.APIList
 import com.example.keepthetime.api.ServerAPI
@@ -42,6 +43,11 @@ class SignInActivity : BaseActivity() {
                     if(response.isSuccessful){
 //                        모든 결과가 최종 성공인 경우. ( code == 200으로 내려옴 )
 //                        response.body() 이용.
+                        val br = response.body()!!  // 성공시 무조건 본문이 있다. => BasicResponse 형태의 변수로 파싱되어 나옴.
+
+//                        Retrofit의 Callback은 UIThread 안으로 다시 돌아오도록 처리되어있다.
+//                        UI 조작을 위해 runOnUiThread {  } 작성 필요 X
+                        Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
                     }
 //                    실패일때는?
                     else{
