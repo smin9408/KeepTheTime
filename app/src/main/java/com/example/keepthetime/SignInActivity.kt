58d7f0c284c79b1a7a9041cd436f1ed260789f51
smin9408 <stmdaks888@naver.com>
2022-03-16 23:48:52 +0900

    로그인 함수 호출 > 서버 기능 Call  - 임시 Callback 등록 : 응답내용만 확인

--- app/src/main/java/com/example/keepthetime/SignInActivity.kt
+++ app/src/main/java/com/example/keepthetime/SignInActivity.kt
@@ -2,10 +2,15 @@ package com.example.keepthetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime.api.APIList
 import com.example.keepthetime.api.ServerAPI
 import com.example.keepthetime.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
 
@@ -20,6 +25,26 @@ class SignInActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnLogin.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject>{
+                override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+
+                    Log.d("응답확인", response.toString())
+                }
+
+                override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+//                    서버에 물리적 연결 실패
+
+
+                }
+
+            })
+
+        }
 
     }
 
