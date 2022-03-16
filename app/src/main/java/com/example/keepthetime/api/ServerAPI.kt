96536abe8f0389a92e729f59c8bede3d061b6738
smin9408 <stmdaks888@naver.com>
2022-03-16 22:41:24 +0900

    JSON 파싱을 쉽게 도와주는 GSON 라이브러리 결합

--- app/src/main/java/com/example/keepthetime/api/ServerAPI.kt
+++ app/src/main/java/com/example/keepthetime/api/ServerAPI.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 
@@ -12,16 +13,18 @@ class ServerAPI {
 //        서버통신담당 클래스 : 레트로핏 클래스 객체를 담을 변수
 //        아직 안만들었다면? 새로 만들고, 만들어둔게 있다면? 있는 retrofit 재활용.
 
-        private var retrofit : Retrofit? = null
+        private var retrofit: Retrofit? = null
         private val BASE_URL = "https://keepthetime.xyz"
 
-        fun getRetrofit() : Retrofit {
+        fun getRetrofit(): Retrofit {
 
 //            Retrofit 라이브러리는, 클래스 차원에서 BASE_URL을 설정할 수 있게 도와줌.
+//            Retrofit + Gson 두개의 라이브러리를 결합하면 => JSON 파싱이 쉬워짐.
 
-            if(retrofit == null){
+            if (retrofit == null) {
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)  // 어느 서버를 기반으로 움직일 건지.
+                    .addConverterFactory(GsonConverterFactory.create())  // Gson 라이브러리와 결합
                     .build()
             }
 
