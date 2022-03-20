a5702a2079a57dc9a0a9e2ce145c6575883ad44d
smin9408 <stmdaks888@naver.com>
2022-03-20 23:18:50 +0900

    연습문제 - 약속목록 프래그먼트에 BaseFragment, 데이터바인딩 세팅 적용

--- app/src/main/java/com/example/keepthetime/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/example/keepthetime/fragments/AppointmentListFragment.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
 import com.example.keepthetime.R
+import com.example.keepthetime.databinding.FragmentAppointmentListBinding
 
-class AppointmentListFragment : Fragment() {
+class AppointmentListFragment : BaseFragment() {
+
+    lateinit var binding: FragmentAppointmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_appointment_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_appointment_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+
+        binding.txtAppointmentListName.text = "약속 목록"
+
     }
 
 }
