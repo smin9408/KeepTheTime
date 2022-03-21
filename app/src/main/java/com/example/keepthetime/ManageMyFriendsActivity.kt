8a04582031819948d5f23f4ac78228c64e3c885b
smin9408 <stmdaks888@naver.com>
2022-03-21 11:18:11 +0900

    내 친구 목록을 ArrayList에 담아주기 시도 중  - 레트로핏으로 내 친구 목록 불러내는 API 호출

--- app/src/main/java/com/example/keepthetime/ManageMyFriendsActivity.kt
+++ app/src/main/java/com/example/keepthetime/ManageMyFriendsActivity.kt
@@ -2,12 +2,20 @@ package com.example.keepthetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.UserData
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime.databinding.ActivityManageMyFriendsBinding
+import com.example.keepthetime.datas.BasicResponse
+import com.example.keepthetime.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ManageMyFriendsActivity : BaseActivity() {
 
     lateinit var binding: ActivityManageMyFriendsBinding
+
+    val mFriendList = ArrayList<UserData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +28,21 @@ class ManageMyFriendsActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        getMyFriendListFromServer()
+    }
+
+    fun getMyFriendListFromServer() {
+
+        apiList.getRequestFriendList(
+            ContextUtil.getLoginUserToken(mContext),
+            "my" // 수락 완료된 친구목록만 불러오기
+        ).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+            }
+        })
+
     }
 }
